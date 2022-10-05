be0f93ba4375cd225e8ef1d8de68de2f589a232f
Eyelessdude <bohdandudka@gmail.com>
2022-10-05 18:09:17 +0200

    Initial commit with project structure

---
+++ src/main/java/com/eyelessdude/learnyourword/LearnYourWordApplication.java
@@ -0,0 +1,13 @@
+package com.eyelessdude.learnyourword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnYourWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnYourWordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eyelessdude/learnyourword/LearnYourWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyelessdude.learnyourword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnYourWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
